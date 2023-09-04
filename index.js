1e4ef6b5f70cea143d309adc2871b27d9e76e917
capp98 <54820067+capp98@users.noreply.github.com>
2023-09-04 15:24:25 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const port = 3010;
+const path = require('path');
+
+app.use(express.static('static'));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve('pages/index.html'));
+});
+
+app.get('/teste', (req, res) => {
+  res.json({ resultado: 'Funcionando perfeitamente!!!' });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
